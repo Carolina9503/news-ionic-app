d1798dfea70f26fc9082f2ea0475f0cbb325d9c5
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-09-24 23:52:27 -0500

    cree el servicio news.service para consumir las noticias de la Api NewaApi.org documentacion aqui https://newsapi.org/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'fbd0e9bf8d9343c49f3b02c4bfb4e36d'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'fbd0e9bf8d9343c49f3b02c4bfb4e36d'
 };
 
 /*
